72a1176475a733d8ec4ae880c6a2361b7051c189
rcrjr97 <82326727+rcrjr97@users.noreply.github.com>
2021-04-11 20:19:08 -0400

    Fixed misspelled import statement

--- PersonalWebsite/urls.py
+++ PersonalWebsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.view.generic import RedirectView
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
