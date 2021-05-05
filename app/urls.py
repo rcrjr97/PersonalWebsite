54720e606936f20cbee6f8272fd65a0023f955da
rcrjr97 <82326727+rcrjr97@users.noreply.github.com>
2021-05-05 10:38:59 -0400

    Change urls.py to include robots.txt

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="personalwebsite/robots.txt", content_type='text/plain')),
 
 
 
